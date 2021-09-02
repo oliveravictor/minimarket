5c169e3ba104ca2b1950dd86a994097d81ab1785
Victor Olivera <ovictoriox@gmail.com>
2021-09-02 00:47:42 -0300

    item detail available

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
   var screenLoad = new bootstrap.Modal(document.getElementById('screenLoad'), {})
 
-  let list = document.getElementById('list')
-  let listGroup = document.getElementById('listGroup')
-  let empty = document.getElementById('empty')
+  let list = document.getElementById('list') 
+  let listGroup = document.getElementById('listGroup') 
+  let empty = document.getElementById('empty') 
+  let detail = document.getElementById('detail') 
 
   let save = document.getElementById('screenSave').addEventListener('click', () => {          
     let product = document.getElementById('screenLoadProduct').value
@@ -13,11 +14,24 @@
     document.getElementById('screenLoadIcon').value = ""
     document.getElementById('screenLoadInfo').value = ""
 
-    let model = `<li class="list-group-item"><img src="${icon}" alt="${product}" class="list__img">${product}</li>`
-    list.innerHTML += model
-    console.log(product, icon, info, list)  
+    let model = `<li class="list-group-item" data-product="${product}" data-icon="${icon}" data-info="${info}"
+    ><img src="${icon}" alt="${product}" class="list__img">${product}</li>`
+    listGroup.innerHTML += model
+    // console.log(product, icon, info, list)  
     screenLoad.hide()
     empty.style.display = 'none'
     list.style.display = 'block'
-
  })
+
+ let buttonClick = document.getElementById('listGroup').addEventListener('click', (e) => {
+   document.getElementById('detailIcon').src = e.target.getAttribute('data-icon')
+   document.getElementById('detailProduct').innerHTML = e.target.getAttribute('data-product')
+   document.getElementById('detailInfo').innerHTML = e.target.getAttribute('data-info')
+   list.style.display = 'none'
+   detail.style.display = 'block'
+})
+
+let detailButton = document.getElementById('detailButton').addEventListener('click', () => {
+  list.style.display = 'block'
+  detail.style.display = 'none'
+})
